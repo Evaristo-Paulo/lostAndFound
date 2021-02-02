69510a784cdd5b12acbc1f498b361596fd985c0a
Evaristo-Paulo <ivarilson909@gmail.com>
2021-02-02 12:41:17 +0100

    Painel: Mudar a dashboard

--- resources/views/painel/dashboard.blade.php
+++ resources/views/painel/dashboard.blade.php
@@ -9,7 +9,23 @@ Painel de controle
         <div class="row">
             <div class="top_tiles col-lg-12">
                 <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 ">
-                    <div class="tile-stats">
+                    <div class="tile-stats" style="background-color: #34B8A1">
+                        <div class="icon"><img src="{{ url('painel/icon/png/008-handshake.png') }}" alt=""></div>
+                        <div class="count">17</div>
+                        <h3>Objectos Entregues</h3>
+                        <p><a href="#">Ver mais</a></p>
+                    </div>
+                </div>
+                <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 ">
+                    <div class="tile-stats" style="background-color: #0099F4">
+                        <div class="icon"><img src="{{ url('painel/icon/png/006-purse.png') }}" alt=""></div>
+                        <div class="count">23</div>
+                        <h3>Objectos Achados</h3>
+                        <p><a href="#">Ver mais</a></p>
+                    </div>
+                </div>
+                <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 ">
+                    <div class="tile-stats" style="background-color: #F4AD01">
                         <div class="icon">
                             <img src="{{  url('painel/icon/png/002-lost-items-1.png') }}" alt="">
                         </div>
@@ -19,57 +35,20 @@ Painel de controle
                     </div>
                 </div>
                 <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 ">
-                    <div class="tile-stats">
-                        <div class="icon"><img src="{{ url('painel/icon/png/006-purse.png') }}" alt=""></div>
-                        <div class="count">23</div>
-                        <h3>Objectos Achados</h3>
-                        <p><a href="#">Ver mais</a></p>
-                    </div>
-                </div>
-                <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 ">
-                    <div class="tile-stats">
-                        <div class="icon"><img src="{{ url('painel/icon/png/008-handshake.png') }}" alt=""></div>
-                        <div class="count">17</div>
-                        <h3>Objectos Entregues</h3>
-                        <p><a href="#">Ver mais</a></p>
-                    </div>
-                </div>
-                <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 ">
-                    <div class="tile-stats">
-                        <div class="icon"><img src="{{ url('painel/icon/png/004-group.png') }}" alt=""></div>
+                    <div class="tile-stats" style="background-color: #EE2C4D">
+                        <div class="icon"><img src="{{ url('painel/icon/png/002-thief.png') }}" alt=""></div>
                         <div class="count">25</div>
-                        <h3>Usuários</h3>
+                        <h3>Objectos Roubados</h3>
                         <p><a href="#">Ver mais</a></p>
                     </div>
-                </div>
+                </div>    
             </div>
         </div>
 
+        {{-- Aqui entra a logo --}}
         <div class="row">
-            <div class="col-md-6 col-sm-6  ">
-                <div class="x_panel">
-                    <div class="x_title">
-                        <h2>Objectos perdidos</h2>
-                        
-                        <div class="clearfix"></div>
-                    </div>
-                    <div class="x_content">
-                        <canvas id="lineChart"></canvas>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-6 col-sm-6  ">
-                <div class="x_panel">
-                    <div class="x_title">
-                        <h2>Objectos achados</h2>
-                        
-                        <div class="clearfix"></div>
-                    </div>
-                    <div class="x_content">
-                        <canvas id="mybarChart"></canvas>
-                    </div>
-                </div>
+            <div class="col-lg-12">
+                
             </div>
         </div>
 
